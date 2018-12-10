27dcb8cbcf3e8c673c1b7ece5a0463c77604122b
Kayleigh-Yeager <44267121+Kayleigh-Yeager@users.noreply.github.com>
2018-12-10 17:15:21 +0000

    team2 clone ky
    submitted a change to the code for a new branch in my fork

--- team2.py
+++ team2.py
@@ -66,3 +66,5 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+    #this is a clone not the droid your looking for
+    
